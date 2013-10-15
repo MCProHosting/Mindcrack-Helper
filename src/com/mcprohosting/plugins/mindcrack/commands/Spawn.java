617804ce73b3e4633de67c25eab874948772d008
mstiles92 <mstiles92@gmail.com>
2013-10-14 20:26:39 -0400

    Implement the spawn command

--- src/com/mcprohosting/plugins/mindcrack/commands/Spawn.java
+++ src/com/mcprohosting/plugins/mindcrack/commands/Spawn.java
@@ -1,8 +1,13 @@
 package com.mcprohosting.plugins.mindcrack.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.mcprohosting.plugins.mindcrack.Mindcrack;
+import com.mcprohosting.plugins.mindcrack.ServerType;
 
 public class Spawn implements CommandExecutor {
 	//Takes the player back to the spawn point from Mindcrack.getSpawn();
@@ -13,8 +18,16 @@ public class Spawn implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (label.equals("spawn")) {
-
+		if (sender instanceof Player) {
+			if (!Mindcrack.getServerType().equals(ServerType.GAME)) {
+				Player player = (Player) sender;
+				
+				player.teleport(Mindcrack.getSpawnLocation());
+			}
+		} else {
+			sender.sendMessage(ChatColor.RED + "This command can only be executed by a player.");
 		}
+		
+		return true;
 	}
 }
