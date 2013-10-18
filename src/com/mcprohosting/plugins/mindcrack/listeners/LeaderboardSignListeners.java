5dfdf2c4f4cc8c30a8eb31a4429cb63715d7fba0
mstiles92 <mstiles92@gmail.com>
2013-10-17 23:55:25 -0400

    Small changes to teleport signs

--- src/com/mcprohosting/plugins/mindcrack/listeners/LeaderboardSignListeners.java
+++ src/com/mcprohosting/plugins/mindcrack/listeners/LeaderboardSignListeners.java
@@ -15,7 +15,7 @@ public class LeaderboardSignListeners implements Listener {
 
 	@EventHandler
 	public void onBlockPlace(BlockPlaceEvent event) {
-		if (event.getBlock().getType().equals(Material.SIGN) && event.getPlayer().hasPermission("mindcrack.editleaderboardsigns")) {
+		if (event.getBlock().getType().equals(Material.WALL_SIGN) && event.getPlayer().hasPermission("mindcrack.editleaderboardsigns")) {
 			Sign sign = (Sign) event.getBlock();
 			if (sign.getLine(0).equalsIgnoreCase("[lb]")) {
 				LeaderboardSigns.addSign(event.getBlock().getLocation());
@@ -27,7 +27,7 @@ public class LeaderboardSignListeners implements Listener {
 	public void onBlockBreak(BlockBreakEvent event) {
 		Block block = event.getBlock();
 		
-		if (block.getType().equals(Material.SIGN) && LeaderboardSigns.isRegistered(block.getLocation())) {
+		if (block.getType().equals(Material.WALL_SIGN) && LeaderboardSigns.isRegistered(block.getLocation())) {
 			if (!event.getPlayer().hasPermission("mindcrack.editleaderboardsigns")) {
 				event.getPlayer().sendMessage(ChatColor.RED + "You do not have permission to remove leaderboard signs.");
 				event.setCancelled(true);
--- src/com/mcprohosting/plugins/mindcrack/teleporters/TeleportSignListeners.java
+++ src/com/mcprohosting/plugins/mindcrack/teleporters/TeleportSignListeners.java
@@ -3,12 +3,11 @@ package com.mcprohosting.plugins.mindcrack.teleporters;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 import com.mcprohosting.plugins.mindcrack.utilities.UtilityMethods;
@@ -17,7 +16,7 @@ public class TeleportSignListeners implements Listener {
 
 	@EventHandler
 	public void onInteract(PlayerInteractEvent event) {
-		if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && event.getClickedBlock().getType().equals(Material.SIGN)) {
+		if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) && event.getClickedBlock().getType().equals(Material.WALL_SIGN)) {
 			if (TeleportSigns.isRegistered(event.getClickedBlock().getLocation())) {
 				String serverName = TeleportSigns.getServerName(event.getClickedBlock().getLocation());
 				if (serverName != null) {
@@ -28,11 +27,10 @@ public class TeleportSignListeners implements Listener {
 	}
 	
 	@EventHandler
-	public void onBlockPlace(BlockPlaceEvent event) {
-		if (event.getBlock().getType().equals(Material.SIGN) && event.getPlayer().hasPermission("mindcrack.editteleportsigns")) {
-			Sign sign = (Sign) event.getBlock();
-			if (sign.getLine(0).matches("\\[tp [1-8]\\]")) {
-				TeleportSigns.addSign(sign.getLine(0).substring(4, 5) + "-kotl", sign.getLocation());
+	public void onSignEdit(SignChangeEvent event) {
+		if (event.getPlayer().hasPermission("mindcrack.editteleportsigns")) {
+			if (event.getLine(0).matches("\\[tp [1-8]\\]")) {
+				TeleportSigns.addSign(event.getLine(0).substring(4, 5) + "-kotl", event.getBlock().getLocation());
 			}
 		}
 	}
@@ -41,7 +39,7 @@ public class TeleportSignListeners implements Listener {
 	public void onBlockBreak(BlockBreakEvent event) {
 		Block block = event.getBlock();
 		
-		if (block.getType().equals(Material.SIGN) && TeleportSigns.isRegistered(block.getLocation())) {
+		if (block.getType().equals(Material.WALL_SIGN) && TeleportSigns.isRegistered(block.getLocation())) {
 			if (!event.getPlayer().hasPermission("mindcrack.editteleportsigns")) {
 				event.getPlayer().sendMessage(ChatColor.RED + "You do not have permission to remove teleport signs.");
 				event.setCancelled(true);
